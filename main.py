8add71789ce664d7c1c535bbe6ec0fc099639d0f
Brad Ashburn <ashburnb@oregonstate.edu>
2022-10-18 00:40:00 -1000

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+
+# create the Flask instance app
+app = Flask(__name__)
+
+# decorator route to home page
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+# decorator route to user page
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', name=name)
+
+
+# Invalid URL Error
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
+
+# Internal Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+	return render_template('500.html'), 500
+
+
+
